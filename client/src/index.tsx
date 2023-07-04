fb72afad64a495b0a5cd0c3bff2570bd2c5a0a6b
Landon Connell <landonbconnell2001@gmail.com>
2023-07-03 20:02:20 -0400

    oops forgot about index.js

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = document.getElementById("root");
+
+if (!element) {
+  throw new Error("Could not find root element");
+}
+
+const root = ReactDOM.createRoot(element);
 root.render(
   <React.StrictMode>
     <App />
