3991199031d4610754f1d68fac20ef51f56e89c9
Landon Connell <landonbconnell2001@gmail.com>
2023-07-03 21:12:09 -0400

    set up Redux Toolkit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { store } from "redux/store";
+import { Provider } from "react-redux";
 
 const element = document.getElementById("root");
 
@@ -11,6 +13,8 @@ if (!element) {
 const root = ReactDOM.createRoot(element);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/redux/reducers/mealsSlice.ts
@@ -0,0 +1,79 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface MealDay {
+  weekday: string;
+}
+
+export interface MealsState {
+  meals: {
+    breakfast: {
+      days: MealDay[];
+    };
+    lunch: {
+      days: MealDay[];
+    };
+    dinner: {
+      days: MealDay[];
+    };
+  };
+}
+
+const defaultWeekdays: MealDay[] = [
+  {
+    weekday: "Monday",
+  },
+  {
+    weekday: "Tuesday",
+  },
+  {
+    weekday: "Wednesday",
+  },
+  {
+    weekday: "Thursday",
+  },
+  {
+    weekday: "Friday",
+  },
+  {
+    weekday: "Saturday",
+  },
+  {
+    weekday: "Sunday",
+  },
+];
+
+const initialState: MealsState = {
+  meals: {
+    breakfast: {
+      days: defaultWeekdays,
+    },
+    lunch: {
+      days: defaultWeekdays,
+    },
+    dinner: {
+      days: defaultWeekdays,
+    },
+  },
+};
+
+export const mealsSlice = createSlice({
+  name: "meals",
+  initialState,
+  reducers: {
+    // increment: (state) => {
+    //   state.value += 1;
+    // },
+    // decrement: (state) => {
+    //   state.value -= 1;
+    // },
+    // incrementByAmount: (state, action: PayloadAction<number>) => {
+    //   state.value += action.payload;
+    // },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {} = mealsSlice.actions;
+
+export default mealsSlice.reducer;
