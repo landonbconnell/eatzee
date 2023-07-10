33feb04fd656f571f48f90a42e5fcfb29b6e38a9
Landon Connell <landonbconnell2001@gmail.com>
2023-07-10 12:15:29 -0400

    added skillLevel field to user slice

--- client/src/redux/reducers/userSlice.ts
+++ client/src/redux/reducers/userSlice.ts
@@ -4,11 +4,13 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 export interface UserState {
   dietaryRestrictions: string[];
   allergies: string[];
+  skillLevel: number;
 }
 
 const initialState: UserState = {
   dietaryRestrictions: [],
   allergies: [],
+  skillLevel: 2,
 };
 
 export const userSlice = createSlice({
@@ -21,11 +23,15 @@ export const userSlice = createSlice({
     setAllergies: (state, action: PayloadAction<string[]>) => {
       state.allergies = action.payload;
     },
+    setSkillLevel: (state, action: PayloadAction<number>) => {
+      state.skillLevel = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
 
-export const { setDietaryRestrictions, setAllergies } = userSlice.actions;
+export const { setDietaryRestrictions, setAllergies, setSkillLevel } =
+  userSlice.actions;
 
 export default userSlice.reducer;
